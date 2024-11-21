3c2136ae4fe7e96e59a11fbca6ec92c43aaad4f1
rawfuntoha <rawfunislam@gmail.com>
2024-11-21 17:38:32 +0600

    test prac

--- tests/test2.spec.js
+++ tests/test2.spec.js
@@ -1,14 +1,46 @@
 const { test, expect } = require('@playwright/test');
 
 test('Log page title to console', async ({ page }) => {
-  // Navigate to a URL
-  await page.goto('https://google.com');
-  
-  // Get the title of the page
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+ 
   const title = await page.title();
   
-  // Log the title to the console
+  
   console.log('Page Title:', title);
-  expect(title).toBe('Google');  
+  //expect(title).toBe('Google');  
 
 });
+
+test('lLog page title to console', async ({ page }) => {
+
+    await page.goto("https://fb.com/");
+   
+    const title = await page.title();
+    
+    
+    console.log('Page Title:', title);
+    //expect(title).toBe('Google');  
+  
+  });
+
+
+// test('Browser context playwright test',async ({browser})=>
+
+//     {
+//         const context = await browser.newContext();
+//         const page = await context.newPage();
+//         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+//         console.log(await page.title());
+//     });
+
+
+//  test.only('page playwright test',async ({page})=>
+
+//         {
+//             //const context = await browser.newContext();
+//             //const page = await context.newPage();
+//             await page.goto("https://google.com");
+//             console.log(await page.title());
+//            // expect(page).toBe("Google");
+    
