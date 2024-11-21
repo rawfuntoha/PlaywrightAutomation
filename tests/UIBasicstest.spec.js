3fff4e684949d1bf4b9c0b529b3f86ad0629f919
rawfuntoha <rawfunislam@gmail.com>
2024-11-21 14:59:35 +0600

    Broswer_Page_open

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,9 +1,17 @@
 const {test} = require('@playwright/test');
 
-test('First playwright test',async function()
+test('Browser context playwright test',async ({browser})=>
 
 {
-    
-}
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
 
-);
+test('page playwright test',async ({page})=>
+
+    {
+        //const context = await browser.newContext();
+        //const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    });
