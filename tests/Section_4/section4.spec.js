294f74e5dc44d319ec8e06a9660dc5115f322ab0
rawfuntoha <rawfunislam@gmail.com>
2024-11-24 14:26:19 +0600

    login

--- tests/Section_4/section4.spec.js
+++ tests/Section_4/section4.spec.js
@@ -4,17 +4,28 @@ const {test,expect} = require('@playwright/test');
 test.only('Browser context playwright test',async ({browser})=>
 
 {
+
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
   const title = await page.title();
   console.log('Page Title:', title);
+  const userName = page.locator('#username');
+  const signIn = page.locator("#signInBtn");
+
   await page.locator('#username').fill("toha");
-  await page.locator("[type='password']").fill("tohapassword");
+ 
+  await page.locator("[type='password']").fill("learning");
   await page.locator('#signInBtn').click();
   console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  
+  await page.waitForTimeout(10000);
 
 
 });
