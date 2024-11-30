abae3d4f4c13b6e810c35aea397d6368ae09dcb8
rawfuntoha <rawfunislam@gmail.com>
2024-11-30 10:44:53 +0600

    sec 4 complete

--- tests/Section_4/assignment.spec.js
+++ tests/Section_4/assignment.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Browser context playwright test',async ({page})=>  {
+test('Browser context playwright test',async ({page})=>  {
     await page.goto("https://rahulshettyacademy.com/client");
      const delay = page.waitForTimeout(10000);
 
@@ -8,11 +8,11 @@ test.only('Browser context playwright test',async ({page})=>  {
     await page.locator('#userPassword').fill("12345@Jatri");
     await page.locator('#login').click();
 
-    await page.waitForLoadState('networkidle')
+    //await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
 
     await delay;
-    //home
-    //mac
+    
   });
