28ce1e166933b09e3358ba8ccbb273f5e0eab518
rawfuntoha <rawfunislam@gmail.com>
2024-11-30 21:58:13 +0600

    add to cart

--- tests/section_7/section7.spec.js
+++ tests/section_7/section7.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 test('Browser playwright test',async ({page})=>  {
 
 
-    const productName = 'zara coat 3';
+    const productName = 'ZARA COAT 3';
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
 
@@ -11,20 +11,19 @@ test('Browser playwright test',async ({page})=>  {
     await page.locator('#userPassword').fill("12345@Jatri");
     await page.locator('#login').click();
 
-    
+
     await page.waitForLoadState('networkidle');
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
-    const count = products.count();
-
-    for(let i=0; i<count; ++i)
-    {
-        if ( await products.nth(i).locator("b").textContent() === productName)
-        {
-            await products.nth(i).locator("text = Add To Cart").click();
-            break;
-        }
+    const count = await products.count();
+    for (let i = 0; i < count; ++i) {
+       if (await products.nth(i).locator("b").textContent() === productName) {
+          await products.nth(i).locator("text= Add To Cart").click();
+          break;
+       }
     }
-    
+
+
+    await page.pause();
     
   });
