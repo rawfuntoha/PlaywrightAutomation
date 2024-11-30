6183b91003784c6c120089bcd2d8bdec9e7c415c
rawfuntoha <rawfunislam@gmail.com>
2024-11-30 21:13:40 +0600

    section 7 assignment

---
+++ tests/section_7/section7.spec.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test');
+
+test('Browser playwright test',async ({page})=>  {
+
+
+    const productName = 'zara coat 3';
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+
+    await page.locator('#userEmail').fill("jatritest@gmail.com");
+    await page.locator('#userPassword').fill("12345@Jatri");
+    await page.locator('#login').click();
+
+    
+    await page.waitForLoadState('networkidle');
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+    const count = products.count();
+
+    for(let i=0; i<count; ++i)
+    {
+        if ( await products.nth(i).locator("b").textContent() === productName)
+        {
+            await products.nth(i).locator("text = Add To Cart").click();
+            break;
+        }
+    }
+    
+    
+  });
