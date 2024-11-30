4a3e30c689699ab90b8254058f75c002d46134d8
rawfuntoha <rawfunislam@gmail.com>
2024-11-30 12:16:45 +0600

    section5 part 1

---
+++ tests/section_5/section5.spec.js
@@ -0,0 +1,31 @@
+const { test, expect } = require('@playwright/test');
+
+test('UI Controls', async ({page})=>
+    {
+       await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+       const userName = page.locator('#username');
+       const signIn = page.locator("#signInBtn");
+       const dropdown = page.locator("select.form-control");
+       await dropdown.selectOption("consult");
+       await page.locator(".radiotextsty").last().click();
+       await page.locator("#okayBtn").click();
+       
+
+       
+       
+       await page.pause();
+
+    //    const documentLink = page.locator("[href*='documents-request']");
+    //    const dropdown = page.locator("select.form-control");
+    //    await dropdown.selectOption("consult");
+    //    await page.locator(".radiotextsty").last().click();
+    //    await page.locator("#okayBtn").click();
+    //    console.log(await page.locator(".radiotextsty").last().isChecked());
+    //    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    //    await page.locator("#terms").click();
+    //    await expect( page.locator("#terms")).toBeChecked();
+    //    await page.locator("#terms").uncheck();
+    //    expect( await page.locator("#terms").isChecked()).toBeFalsy();
+    //    await expect(documentLink).toHaveAttribute("class","blinkingText");
+    });
+   
